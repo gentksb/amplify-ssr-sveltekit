cb9d6908f1c09772e429292a8c746ec12f0cc95d
takagen <takagen@amazon.co.jp>
2024-05-13 20:14:00 +0900

    実行環境用package.json

--- bin/postbuild.sh
+++ bin/postbuild.sh
@@ -8,5 +8,5 @@ cp -r ./build ./.amplify-hosting/compute/default
 
 cp -r ./static ./.amplify-hosting
 cp -r ./node_modules ./.amplify-hosting/compute/default/node_modules
-
+cp -r bin/package.json ./.amplify-hosting/compute/default
 cp deploy-manifest.json ./.amplify-hosting/deploy-manifest.json
